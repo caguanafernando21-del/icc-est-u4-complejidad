3a0a6696e6fc0cfb13ce0d93f8cb993b01bfb811
Fernando caguana <fernandocaguana31@gmail.com>
2026-04-14 19:41:23 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hola mundo!");
         
-        int[] array = new int[] {1,2,3,4,5};
+        int[] array = new int[] {7,9,2,3,5};
         System.out.println(array[0]);
         System.out.println(array);
         // for normal posiciones
